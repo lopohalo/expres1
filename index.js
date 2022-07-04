1c6aa24769af66ab96553c12316ba27641b57b7b
SrJuan <jsebg.ruedat@gmail.com>
2022-07-03 21:27:58 -0500

    1

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const conectarBaseDeDatos = require('./config/db')
-const cors = require('cors');
+
 const aplicacion = express();  
 conectarBaseDeDatos()
-aplicacion.use(cors());
+
 aplicacion.use(express.json());
 
 aplicacion.use('/api', require('./routes/proyecto_rutas'))
