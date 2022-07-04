f41a432afabf01765a5b6fa1521b35a01cb80940
SrJuan <jsebg.ruedat@gmail.com>
2022-07-03 22:05:04 -0500

    1

--- index.js
+++ index.js
@@ -1,13 +1,25 @@
+import cors from 'cors'
 const express = require('express');
 const conectarBaseDeDatos = require('./config/db')
 
 const aplicacion = express();  
 conectarBaseDeDatos()
+const whitelist = [process.env.Fronted]
+const corsoptions = {
+    origin: function(origin, callback) {
+        if(whitelist.includes(origin)){
+            callback(null, true)
+        }else {
+            callback(new Error('error de cors'))
+        }
+    }
+}
+app.use(cors(corsoptions));
 
 aplicacion.use(express.json());
 
 aplicacion.use('/api', require('./routes/proyecto_rutas'))
 
 aplicacion.listen(process.env.PORT || 80, () => {
-    console.log("Listening on port 80");
-  });
+  console.log("Listening on port 80");
+});
