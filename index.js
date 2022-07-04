34876cf24953ab8c4de913147a759e3e23c3ea8a
SrJuan <jsebg.ruedat@gmail.com>
2022-07-03 22:57:57 -0500

    1

--- index.js
+++ index.js
@@ -6,18 +6,18 @@ const conectarBaseDeDatos = require('./config/db')
 
 const aplicacion = express();  
 conectarBaseDeDatos()
-const whitelist = ["https://effulgent-klepon-812c50.netlify.app"]
-const corsoptions = {
-    origin: function(origin, callback) {
-        console.log(origin)
-        if(whitelist.includes(origin)){
-            callback(null, true)
-        }else {
-            callback(true)
-        }
-    }
-}
-aplicacion.use(cors(corsoptions));
+// const whitelist = ["https://effulgent-klepon-812c50.netlify.app"]
+// const corsoptions = {
+//     origin: function(origin, callback) {
+//         console.log(origin)
+//         if(whitelist.includes(origin)){
+//             callback(null, true)
+//         }else {
+//             callback(true)
+//         }
+//     }
+// }
+aplicacion.use(cors());
 
 aplicacion.use(express.json());
 
